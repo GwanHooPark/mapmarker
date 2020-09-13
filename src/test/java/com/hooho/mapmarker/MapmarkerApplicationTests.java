7173c33ef66192a214b57769ce69f2c548baaf37
hooho <hooho.dev@gmail.com>
2020-09-13 10:10:15 +0900

    Initial commit

---
+++ src/test/java/com/hooho/mapmarker/MapmarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hooho.mapmarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapmarkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
